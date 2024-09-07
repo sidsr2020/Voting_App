8aebedbb42ea2059729565ec0690d3e7b9a4d9a5
sidsr2020 <106577774+sidsr2020@users.noreply.github.com>
2024-09-07 22:11:57 +0530

    next version of project

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //const bcrypt = require('bcrypt');
-// Define the Person schema
+// Define the user schema
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
