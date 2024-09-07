0f98fba4f170d365e9b3af5a6b3632e7e147ed2c
sidsr2020 <106577774+sidsr2020@users.noreply.github.com>
2024-09-07 20:53:34 +0530

    first version of our voting application

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+//const bcrypt = require('bcrypt');
+// Define the Person schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: String
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    aadharCardNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['voter', 'admin'],
+        default: 'voter'
+    },
+    isVoted: {
+        type: Boolean,
+        default: false
+    }
+});
+const user = mongoose.model('user', userSchema);
+module.exports = user;
